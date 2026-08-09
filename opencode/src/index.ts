5e89dca60abc8d65a01ba0b880f267abcf1d9e73
Khoirul Asfian <mk.asfian@gmail.com>
2026-08-09 15:53:41 +0700

    release: 0.5.0

--- opencode/src/index.ts
+++ opencode/src/index.ts
@@ -15,7 +15,7 @@ import { getConfig } from "../../pi/src/config.js";
 import { createTelemetryCollector } from "../../pi/src/telemetry.js";
 
 const SERVICE_NAME = "pi-otlp-opencode";
-const VERSION = "0.4.1";
+const VERSION = "0.5.0";
 
 export default Plugin.define({
   id: "rulasfia.agents-telemetry",
